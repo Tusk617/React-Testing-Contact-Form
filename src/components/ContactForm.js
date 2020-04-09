277c010b0af0535268495f44491b53ae9e8d8ad7
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-04-09 17:39:34 -0400

    Got my test to show whether or not the error for first name length was showing

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -17,11 +17,11 @@ const ContactForm = () => {
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
-            placeholder="bill"
-            ref={register({ required: true, maxLength: 3 })}
+            placeholder="First name"
+            ref={register({ required: true, maxLength: 10 })}
           />
           {errors.firstName && (
-            <p>Looks like there was an error: {errors.firstName.type}</p>
+            <p data-testid="firstNameError">Looks like there was an error: {errors.firstName.type}</p>
           )}
         </div>
 
@@ -29,7 +29,7 @@ const ContactForm = () => {
           <label htmlFor="lastName">Last Name*</label>
           <input
             name="lastName"
-            placeholder="luo"
+            placeholder="Last name"
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -41,21 +41,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" placeholder="Email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea name="message" placeholder="Please include a message" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid="submitButton" type="submit" />
       </form>
     </div>
   );
